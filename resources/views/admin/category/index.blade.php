4f7940374fa9102e696014f3052e405b857d128b
Mahiuddin <mahiuddin.noyon@gmail.com>
2022-06-22 04:04:49 +0600

    added key count

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -34,6 +34,13 @@
                             <td>
                                 <div class="d-flex px-2 py-1">
                                     <div class="d-flex flex-column justify-content-center">
+                                        <h6 class="mb-0 text-sm"> {{$key+1}} </h6>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="d-flex px-2 py-1">
+                                    <div class="d-flex flex-column justify-content-center">
                                         <h6 class="mb-0 text-sm"> {{$category->name}} </h6>
                                     </div>
                                 </div>
