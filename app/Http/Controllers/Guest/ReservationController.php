ce9228172d468ca64a9595ac9b2c5d24e9cf1bc1
Mahiuddin <mahiuddin.noyon@gmail.com>
2022-07-25 02:06:15 +0600

    added view file for reservation index

--- app/Http/Controllers/Guest/ReservationController.php
+++ app/Http/Controllers/Guest/ReservationController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\Booking;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Room;
 
 class ReservationController extends Controller
 {
     public function index()
     {
-        return view('client.reservation.index');
+        $reservations = Booking::all();
+        return view('client.reservation.index', compact('reservations'));
     }
 }
--- resources/views/client/reservation/index.blade.php
+++ resources/views/client/reservation/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title','Room')
+@section('title','Reservations')
 
 @push('css')
 
@@ -12,7 +12,7 @@
     <div class="card my-4">
         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
             <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                <h6 class="text-white text-capitalize ps-3">Room table</h6>
+                <h6 class="text-white text-capitalize ps-3">My Reservations</h6>
             </div>
         </div>
         <div class="card-body px-0 pb-2">
@@ -25,15 +25,15 @@
                             <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Room Name</th>
                             <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Room Category</th>
                             <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Room Images</th>
-                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">bed</th>
-                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">persion</th>
+                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Checkin Date</th>
+                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Checkout Date</th>
                             <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Price</th>
                             <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Action</th>
                             <th class="text-secondary opacity-7"></th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($rooms as $key=>$room)
+                        @foreach($reservations as $key=>$reservations)
                         <tr>
                             <td>
                                 <div class="d-flex px-2 py-1">
@@ -45,38 +45,38 @@
                             <td>
                                 <div class="d-flex px-2 py-1">
                                     <div class="d-flex flex-column justify-content-center">
-                                        <h6 class="mb-0 text-sm"> {{$room->name}} </h6>
+                                        <h6 class="mb-0 text-sm"> {{$reservation->room->name}} </h6>
                                     </div>
                                 </div>
                             </td>
                             <td>
-                                <p class="text-xs font-weight-bold mb-0">{{$room->category->name}}</p>
+                                <p class="text-xs font-weight-bold mb-0">{{$reservation->room->category->name}}</p>
                             </td>
                             <td>
-                                <img class="img-thumbnail" src="{{url('uploads/room/', $room->image)}}" alt="image" style="height: 70px; width:90px;">
+                                <img class="img-thumbnail" src="{{url('uploads/room/', $reservation->room->image)}}" alt="image" style="height: 70px; width:90px;">
                             </td>
                             <td>
-                                <p class="text-xs font-weight-bold mb-0">{{$room->bed}}</p>
+                                <p class="text-xs font-weight-bold mb-0">{{$reservation->checkin_date}}</p>
                             </td>
                             <td>
-                                <p class="text-xs font-weight-bold mb-0">{{$room->person}}</p>
+                                <p class="text-xs font-weight-bold mb-0">{{$reservation->checkout_date}}</p>
                             </td>
                             <td>
-                                <p class="text-xs font-weight-bold mb-0">{{$room->price}}$</p>
+                                <p class="text-xs font-weight-bold mb-0">{{$reservation->price}}$</p>
                             </td>
 
 
 
                             <td>
-                                <a href="{{ route('admin.room.edit',$room->id) }}" class="btn btn-info btn-sm">Edit</a>
+                                <a href="{{ route('admin.reservation.edit',$reservation->id) }}" class="btn btn-info btn-sm">Edit</a>
 
-                                <form id="delete-form-{{ $room->id }}" action="{{ route('admin.room.destroy',$room->id) }}" style="display: none;" method="POST">
+                                <form id="delete-form-{{ $reservation->id }}" action="{{ route('admin.room.destroy',$reservation->id) }}" style="display: none;" method="POST">
                                     @csrf
                                     @method('DELETE')
                                 </form>
                                 <button type="button" class="btn btn-danger btn-sm" onclick="if(confirm('Are you sure? You want to delete this?')){
                                                     event.preventDefault();
-                                                    document.getElementById('delete-form-{{ $room->id }}').submit();
+                                                    document.getElementById('delete-form-{{ $reservation->id }}').submit();
                                                 }else {
                                                     event.preventDefault();
                                                         }">Delete</button>
