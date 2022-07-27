03bbaa9e6939b641bbe2b3ef2b519101fd3770a9
Mahiuddin <mahiuddin.noyon@gmail.com>
2022-07-28 04:43:38 +0600

    working on dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
+use App\Contact;
+use App\Facilitiy;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Room;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard'); 
+        $room = Room::all();
+        $available_room = Room::where('is_available',true)->get();
+        $category = Category::all();
+        $contact = Contact::where('status',0)->get();
+        $total_contact = Contact::all();
+        $facility = Facilitiy::all();
+
+        return view('admin.dashboard', compact('room','available_room','category','contact','facility','total_contact')); 
     }
 }
